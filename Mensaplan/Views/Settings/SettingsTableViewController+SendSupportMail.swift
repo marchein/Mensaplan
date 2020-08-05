1b6397849a44768f12ed2f4edd1b9d629355974c
Marc Hein <dev@marc-hein.de>
2020-08-05 17:11:53 +0200

    Removed block from using mail in catalyst app

--- Mensaplan/Views/Settings/SettingsTableViewController+SendSupportMail.swift
+++ Mensaplan/Views/Settings/SettingsTableViewController+SendSupportMail.swift
@@ -13,10 +13,6 @@ import UIKit
 // MARK:- Mail Extension
 extension SettingsTableViewController: MFMailComposeViewControllerDelegate {
     func sendSupportMail() {
-        #if targetEnvironment(macCatalyst)
-        let message = "Bei Fragen und Anregungen kannst uns jeder Zeit unter %@ erreichen."
-        showMessage(title: "Support Anfrage", message: String(format: message, MensaplanApp.mailAdress), on: self)
-        #else
         if MFMailComposeViewController.canSendMail() {
             let mail = MFMailComposeViewController()
             mail.mailComposeDelegate = self
@@ -29,7 +25,6 @@ extension SettingsTableViewController: MFMailComposeViewControllerDelegate {
             let mailErrorMessage = "Es ist kein E-Mail Konto in Apple Mail hinterlegt. Bitte kontaktiere uns unter %@"
             showMessage(title: "Fehler", message: String(format: mailErrorMessage, MensaplanApp.mailAdress), on: self)
         }
-        #endif
     }
     
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
